8cc6fbd47a4fc8292d491db9e34c4eb526a48775
James Fairbairn <jaybono30@hotmail.com>
2017-02-16 18:03:02 +0000

    Add Initial Http Get Call to Find Customers By Id

--- customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/src/app/components/customer-detail/customer-detail.ts
+++ customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/src/app/components/customer-detail/customer-detail.ts
@@ -1,14 +1,17 @@
 import {Component} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Customer, CustomerService} from "../../services/customer-service";
 
 @Component({
   selector: 'customer-detail-page',
   templateUrl: 'customer-detail.html'
 })
 export default class CustomerDetailComponent {
-  customerId: string;
+  customer: Customer;
 
-  constructor(route: ActivatedRoute){
-    this.customerId = route.snapshot.params['id'];
+  constructor(route: ActivatedRoute, customerService: CustomerService){
+    this.customer = new Customer();
+    let customerId: number = route.snapshot.params['id'];
+    customerService.getCustomerById(customerId).subscribe(data => this.customer = data);
   }
 }
--- customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/src/app/services/customer-service.ts
+++ customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/src/app/services/customer-service.ts
@@ -4,62 +4,28 @@ import {Observable} from "rxjs";
 
 export class Customer {
     constructor(
-        public id: number,
-        public firstName: string,
-        public lastName: string) {}
+        public id?: number,
+        public firstName?: string,
+        public lastName?: string) {}
 }
 
 @Injectable()
 export class CustomerService {
+  private findByIdUrl = 'http://localhost:3333/customer-service/customer/id';
   private findAllUrl = 'http://localhost:3333/customer-service/customer/findAll';
 
   constructor(private http:Http) {}
 
-    // getCustomers(): Array<Customer> {
-    //     return customers.map(p => new Customer(p.id, p.firstName, p.lastName));
-    // }
-
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error);
-    return Promise.reject(error.message || error);
-  }
-
   getCustomers() : Observable<Customer[]>{
-    return this.http.get(this.findAllUrl).map((res:Response) => res.json())
+    return this.http.get(this.findAllUrl)
+      .map((res:Response) => res.json())
       .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
   }
 
+  getCustomerById(customerId: number) : Observable<Customer>{
+    return this.http.get(this.findByIdUrl+'/'+customerId)
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
 }
 
-var customers = [
-    {
-        "id": 0,
-        "firstName": "FirstName0",
-        "lastName": "LastName0"
-    },
-    {
-        "id": 1,
-        "firstName": "FirstName1",
-        "lastName": "LastName1"
-    },
-    {
-        "id": 2,
-        "firstName": "FirstName2",
-        "lastName": "LastName2"
-    },
-    {
-        "id": 3,
-        "firstName": "FirstName3",
-        "lastName": "LastName3"
-    },
-    {
-        "id": 4,
-        "firstName": "FirstName4",
-        "lastName": "LastName4"
-    },
-    {
-        "id": 5,
-        "firstName": "FirstName5",
-        "lastName": "LastName5"
-    }
-];
