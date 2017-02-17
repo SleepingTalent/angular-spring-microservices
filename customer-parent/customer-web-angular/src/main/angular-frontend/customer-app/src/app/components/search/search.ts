6cc24559df0ba3577d5794213313f41de6cc665b
James Fairbairn <jaybono30@hotmail.com>
2017-02-17 10:59:42 +0000

    Added added validation to firstname search feature

--- customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/src/app/components/search/search.ts
+++ customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/src/app/components/search/search.ts
@@ -1,7 +1,26 @@
 import {Component} from '@angular/core';
+import {CustomerService} from "../../services/customer-service";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'customer-search',
+  providers: [CustomerService],
   templateUrl: 'search.html'
 })
-export default class SearchComponent {}
+export default class SearchComponent {
+  formModel: FormGroup;
+
+  constructor(private customerService : CustomerService) {
+    const formBuilder = new FormBuilder();
+
+    this.formModel = formBuilder.group({
+      'lastName': [null, Validators.minLength(3)]
+    })
+  }
+
+  onSearch() {
+    if (this.formModel.valid) {
+      console.log(this.formModel.value);
+    }
+  }
+}
