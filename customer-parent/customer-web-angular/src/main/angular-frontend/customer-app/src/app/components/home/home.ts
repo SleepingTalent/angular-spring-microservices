0cca09195cb07a09d4881abc8c3aa5f8a69ddfe4
James Fairbairn <jaybono30@hotmail.com>
2017-02-20 09:47:41 +0000

    Added added search functionality

--- customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/src/app/components/home/home.ts
+++ customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/src/app/components/home/home.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
-import {FormControl} from "@angular/forms";
 import {CustomerService, Customer} from '../../services/customer-service';
-import {FilterPipe} from '../pipes/filter-pipe'
 import 'rxjs/add/operator/debounceTime';
 import {Observable} from "rxjs";
 import {SearchEventService} from "../../services/search-event-service";
@@ -13,17 +11,10 @@ import {SearchEventService} from "../../services/search-event-service";
 })
 export default class HomeComponent {
   customers: Observable<Customer[]>;
-  //lastNameFilter: FormControl = new FormControl();
-  //filterCriteria: string;
 
   constructor(private customerService: CustomerService, private searchEventService: SearchEventService) {
 
     this.customers = this.customerService.getCustomers();
-
-    // this.lastNameFilter.valueChanges.debounceTime(100).subscribe(
-    //   value => this.filterCriteria = value,
-    //   error => console.error(error)
-    // )
 
     this.searchEventService.getSearchLastNameEvent()
       .subscribe(
