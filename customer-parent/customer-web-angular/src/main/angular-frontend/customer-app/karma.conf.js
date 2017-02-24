65e546c9c94e8b8424d304bb60cd4006d93ba061
Steve Blance <stephen.blance@gmail.com>
2017-02-24 10:39:15 +0000

    Work on unit testing using Karma (test execution temporarily commented out in pom though) - SB

--- customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/karma.conf.js
+++ customer-parent/customer-web-angular/src/main/angular-frontend/customer-app/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', 'angular-cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-firefox-launcher'),
       require('karma-remap-istanbul'),
       require('angular-cli/plugins/karma')
@@ -38,7 +38,26 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
-    singleRun: false
+    browsers: ['PhantomJS', 'PhantomJS_custom'],
+
+    // you can define custom flags
+    customLaunchers: {
+      'PhantomJS_custom': {
+        base: 'PhantomJS',
+        options: {
+          windowName: 'my-window',
+          settings: {
+            webSecurityEnabled: false
+          },
+        },
+        flags: ['--load-images=true'],
+        debug: true
+      }
+    },
+
+    phantomjsLauncher: {
+      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+      exitOnResourceError: true
+    }
   });
 };
